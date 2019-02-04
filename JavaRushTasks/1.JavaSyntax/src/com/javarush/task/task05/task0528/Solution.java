8c4ba9a7694af329d5ea05879617034c9fb1c56a
Naglezh <savinov_vu@mail.ru>
2019-02-04 11:02:42 +0800

    вернуть строки n раз

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task05/task0528/Solution.java
@@ -6,14 +6,36 @@ package com.javarush.task.task05.task0528;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) {
+        int a = 0;
+
+        int b = 5;
+        b = b + 1;
+
+        int c = getZ(a, b);
+
+        String str;
+
+        System.out.println(c);
         //напишите тут ваш код
-        DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-        df.setLenient(true);
-        Date date = df.parse(request.getParameter("date_parameter"));
-        String formattedDate = df.format(date);
+
+        int z = getZ(a, 5);
+
     }
+
+    private static int getZ(int a, int i) {
+        a = getA(a, 1);
+        return getA(a, i);
+    }
+
+    private static int getA(int a, int i) {
+        return a + i;
+    }
+
+
+
 }
