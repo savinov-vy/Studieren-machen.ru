f8eb2a229dee7d295db47e2807d6ed3627090e60
Naglezh <savinov_vu@mail.ru>
2019-02-16 23:33:06 +0800

    Объекты - семья. Создать переопределить toString. Создать конструкторы

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0723/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task07/task0723/Solution.java
@@ -6,13 +6,13 @@ package com.javarush.task.task07.task0723;
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
-        for (int i = 30; i >= 0; i--) {
+        for (int i = 10; i >= 0; i--) {
             System.out.println(i);
 
             //напишите тут ваш код
-            Thread.sleep(100);
+            Thread.sleep(1000);
         }
 
-        System.out.println("Бум!");
+        System.out.println("Оля Привет!!!! ПЫДЫДЫЩЩЩЩ!!!!");
     }
 }
