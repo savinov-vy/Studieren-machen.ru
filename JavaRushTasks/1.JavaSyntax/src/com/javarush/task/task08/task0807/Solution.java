3777b5104d58e0637c93e069f8f583f16e4fab1e
Naglezh <savinov_vu@mail.ru>
2019-02-22 21:59:20 +0800

    Создать ArrayList и LinkedList

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0807/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task08.task0807;
+
+import java.util.*;
+
+/* 
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        //напишите тут ваш код
+        List<String> list = new ArrayList<>();
+        return  list;
+
+    }
+
+    public static Object createLinkedList() {
+        //напишите тут ваш код
+        List<String> list = new LinkedList<>();
+        return list;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
