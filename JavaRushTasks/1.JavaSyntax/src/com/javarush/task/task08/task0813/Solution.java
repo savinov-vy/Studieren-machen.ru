e371820b164dd338a7de02396d8905f046845467
Naglezh <savinov_vu@mail.ru>
2019-02-23 10:20:24 +0800

    HashSet. Создать, заполнить строками.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //напишите тут ваш код
+        Set<String> set = new HashSet<String>();
+        set.add("Лак");
+        set.add("Лад");
+        set.add("Лаз");
+        set.add("Лапа");
+        set.add("Лава");
+        set.add("Лоб");
+        set.add("Луч");
+        set.add("Луг");
+        set.add("Лов");
+        set.add("Лом");
+        set.add("Лось");
+        set.add("Лунь");
+        set.add("Луна");
+        set.add("Лужа");
+        set.add("Лоск");
+        set.add("Ладонь");
+        set.add("Лайка");
+        set.add("Лавка");
+        set.add("Лампа");
+        set.add("Лодка");
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
