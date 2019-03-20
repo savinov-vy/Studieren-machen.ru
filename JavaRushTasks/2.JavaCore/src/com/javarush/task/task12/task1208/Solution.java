779705ff84a42e03f8cf761f479af2c5a56cdda8
Naglezh <savinov_vu@mail.ru>
2019-03-20 08:24:32 +0800

    Перегрузка методов. Перегрузить 5 методов с разными параметрами

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+//Написать пять методов print с разными параметрами.
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print() {
+
+    }
+
+    public static void print(String d, String e, int a, int b, int c) {
+
+    }
+
+    public static void print(String d, int a, int b, int c, String z) {
+
+    }
+
+    public static void print(String d, int a, int b) {
+
+    }
+
+    public static void print(int a, String d, int b, int c) {
+
+    }
+
+
+}
