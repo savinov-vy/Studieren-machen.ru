19a85dd35d774d6ded03964a4cc722edbd193d34
Naglezh <savinov_vu@mail.ru>
2019-03-20 08:58:31 +0800

    Перегрузка методов. Написать 3 метода по выбору минимального значения пары int. пары long, пары Double.

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+/*
+        Написать public static методы: int min(int, int), long min(long, long), double min(double, double).
+        Каждый метод должен возвращать минимальное из двух переданных в него чисел.*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+    }
+    //Напишите тут ваши методы
+
+    public static int min(int a, int b) {
+        int min;
+        if (a <= b) {
+            min = a;
+        } else min = b;
+        return min;
+    }
+    public static long min(long a, long b) {
+        long min;
+        if (a <= b) {
+            min = a;
+        }
+        else min = b;
+        return  min;
+    }
+
+    public static double min(double a, double b) {
+        double min;
+        if (a <= b) {
+            min = a;
+        }
+        else min = b;
+        return min;
+    }
+
+
+
+}
