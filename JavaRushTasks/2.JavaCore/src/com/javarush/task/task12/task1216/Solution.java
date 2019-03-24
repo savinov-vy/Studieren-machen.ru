5b7dc3cc2b416478cc534d884db9844e1d2d27f8
Naglezh <savinov_vu@mail.ru>
2019-03-24 20:31:19 +0800

    Интерфейс. Написать интерфейс. Добавить 2 метода

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1216/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task12.task1216;
+
+/* 
+Летать охота
+*/
+/*Напиши свой public интерфейс CanFly(летать).
+        Добавь в него два метода.*/
+
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void Fly();
+
+        int SpeedVolume();
+    }
+    //add an interface here - добавь интерфейс тут
+
+}
