af1e978ddf0be175c7a5f0665e27e9b702ca06ec
Naglezh <savinov_vu@mail.ru>
2019-05-30 13:50:47 +0800

    Классы.Интерфейсы.Методы.Взаимодействие. Комплексная задача. Куриная фабрика

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task14/task1408/UkrainianHen.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task14.task1408;
+
+public class UkrainianHen extends Hen {
+    @Override
+    int getCountOfEggsPerMonth() {
+        return 26;
+    }
+
+    @Override
+    String getDescription()  {
+        return  super.getDescription() + " Моя страна - " + Country.UKRAINE + ". Я несу "+ this.getCountOfEggsPerMonth() +" яиц в месяц.";    }
+}
