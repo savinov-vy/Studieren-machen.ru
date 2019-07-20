a47e33cba4f18bbab3af7465f67a3e4a156b2f7e
Naglezh <savinov_vu@mail.ru>
2019-07-20 15:22:49 +0800

    Конструкторы. Создать и перегрузить конструкторы

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    public SubSolution(int a, String s) {
+        super(a, s);
+    }
+
+    protected SubSolution(String s) {
+        super(s);
+    }
+
+    SubSolution(int a) {
+        super(a);
+    }
+}
